e150c7a33ccb67e62b84d5f05e92a263e06cacde
wilberh <wilberhdez@yahoo.com>
2022-06-29 04:28:32 +0000

    updating setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from gettext import install
 from setuptools import find_packages, setup
 
 setup(
-    name='saas',
+    name='src/saas/',
     version='1.0.0.',
     packages=find_packages(),
     include_package_data=True,
